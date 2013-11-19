c16027980674e4c62e58b70b663febb9ff600176
Harry Pujols <mail@harrypujols.com>
2013-11-19 14:24:27 -0500

    opinionated defaults

---
+++ js/main.js
@@ -0,0 +1,69 @@
+(function() {
+  var compare, randomChoice;
+
+  randomChoice = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  compare = function(choice1, choice2) {
+    var result;
+    result = function(outcome) {
+      if (outcome === 'win') {
+        outcome = 'You win!';
+      }
+      if (outcome === 'lose') {
+        outcome = 'You lose :(';
+      }
+      if (outcome === 'tie') {
+        outcome = 'It\'s a tie!';
+      }
+      $('#result').append('<h2>' + outcome + '</h2>');
+    };
+    if (choice1 === choice2) {
+      result('tie');
+    }
+    if (choice1 === 'rock') {
+      if (choice2 === 'paper') {
+        result('lose');
+      }
+      if (choice2 === 'scissors') {
+        result('win');
+      }
+    }
+    if (choice1 === 'paper') {
+      if (choice2 === 'rock') {
+        result('win');
+      }
+      if (choice2 === 'scissors') {
+        result('lose');
+      }
+    }
+    if (choice1 === 'scissors') {
+      if (choice2 === 'rock') {
+        result('lose');
+      }
+      if (choice2 === 'paper') {
+        result('win');
+      }
+    }
+  };
+
+  $('#selection button').click(function() {
+    var computerChoice, userChoice;
+    computerChoice = randomChoice(1, 3);
+    if (computerChoice === 1) {
+      computerChoice = 'rock';
+    }
+    if (computerChoice === 2) {
+      computerChoice = 'paper';
+    }
+    if (computerChoice === 3) {
+      computerChoice = 'scissors';
+    }
+    userChoice = $(this).attr('class');
+    $('#result').empty();
+    $('#result').append('<p> You chose ' + userChoice + '. The computer chose ' + computerChoice + '.</p>');
+    compare(userChoice, computerChoice);
+  });
+
+}).call(this);
